cd10b5ebd9abd88478e155cbb01ed6bb6f081c73
maxiarrojo0612-lang <maxiarrojo0612@gmail.com>
2026-06-30 18:31:33 -0300

    correccionYmapa

--- negro.c
+++ negro.c
@@ -11,22 +11,19 @@
 #define VELOCIDAD 8
 #define ALTO 23
 #define ANCHO 38
-struct item_
-{
+
+struct item_ {
     char nombre[10];
     int consumir;
-
 };
 typedef struct item_ item;
 
-struct mochila_
-{
+struct mochila_ {
     item items[10];
 };
 typedef struct mochila_ mochila;
 
-struct personaje_
-{
+struct personaje_ {
     char nombre[10];
     int vida;
     float X;
@@ -38,37 +35,22 @@ struct personaje_
 };
 typedef struct personaje_ personaje;
 
-
-void cargarMapa(char mapa[ALTO][ANCHO]);
-void leerMapa(char mapa[ALTO][ANCHO], float *posX, float *posY, float nuevaX, float nuevaY);
+void cargarMapa(char mapa[ALTO][ANCHO], float *posX, float *posY);
+void colision(char mapa[ALTO][ANCHO], float *posX, float *posY, float nuevaX, float nuevaY);
 void dibujarMapa(char mapa[ALTO][ANCHO]);
 
-
 int main() {
-
     float posX = TAMANO_CUADRADO; 
     float posY = TAMANO_CUADRADO;
 
-    bool tecla_arriba = false, tecla_abajo = false, tecla_izq = false, tecla_der = false;
     bool salir = true;
 
     char mapa[ALTO][ANCHO];
-    cargarMapa(mapa,&posX,&posY);
-
-    for (int i = 0; i < ALTO; i++) {
-        for (int j = 0; j < ANCHO; j++) {
-            putchar(mapa[i][j]);
-        }
-        putchar('\n');
-    }
+    cargarMapa(mapa, &posX, &posY);
 
     if (!al_init()) return -1;
     al_install_keyboard();
     al_init_primitives_addon();
-
-    al_set_new_display_option(ALLEGRO_OPENGL_MAJOR_VERSION, 2, ALLEGRO_REQUIRE);
-    al_set_new_display_option(ALLEGRO_OPENGL_MINOR_VERSION, 0, ALLEGRO_REQUIRE);
-    al_set_new_display_flags(ALLEGRO_WINDOWED);
 
     ALLEGRO_DISPLAY *display = al_create_display(ANCHO_PANTALLA, ALTO_PANTALLA);
     if (!display) {
@@ -85,54 +67,44 @@ int main() {
     al_register_event_source(queue, al_get_keyboard_event_source());
     al_register_event_source(queue, al_get_timer_event_source(timer));
 
-
     al_start_timer(timer);
 
-    while (salir) {
-        ALLEGRO_EVENT evento;
-        al_wait_for_event(queue, &evento);
+    ALLEGRO_KEYBOARD_STATE teclado;
 
-        if (evento.type == ALLEGRO_EVENT_DISPLAY_CLOSE) salir = false;
+    al_get_keyboard_state(&teclado);
 
-        if (evento.type == ALLEGRO_EVENT_TIMER) {
-            float nuevaX = posX;
-            float nuevaY = posY;
+    al_key_down(&teclado, ALLEGRO_KEY_W);
+    al_key_down(&teclado, ALLEGRO_KEY_S);
+    al_key_down(&teclado, ALLEGRO_KEY_A);
+    al_key_down(&teclado, ALLEGRO_KEY_D);
+    al_key_down(&teclado, ALLEGRO_KEY_ESCAPE);
 
-            if (tecla_arriba) nuevaY -= VELOCIDAD;
-            if (tecla_abajo)  nuevaY += VELOCIDAD;
-            if (tecla_izq)    nuevaX -= VELOCIDAD;
-            if (tecla_der)    nuevaX += VELOCIDAD;
+   while (salir) {
+    ALLEGRO_EVENT evento;
+    al_wait_for_event(queue, &evento);
 
-            leerMapa(mapa, &posX, &posY, nuevaX, nuevaY);
+    if (evento.type == ALLEGRO_EVENT_DISPLAY_CLOSE) salir = false;
+    if (evento.type == ALLEGRO_EVENT_TIMER) {
+        float nuevaX = posX;
+        float nuevaY = posY;
 
-            if (posX < 0) posX = 0;
-            if (posY < 0) posY = 0;
-            if (posX > ANCHO_PANTALLA - TAMANO_CUADRADO) posX = ANCHO_PANTALLA - TAMANO_CUADRADO;
-            if (posY > ALTO_PANTALLA - TAMANO_CUADRADO) posY = ALTO_PANTALLA - TAMANO_CUADRADO;
+        al_get_keyboard_state(&teclado);
 
-            al_clear_to_color(al_map_rgb(10, 10, 10));
-            dibujarMapa(mapa);
-            al_draw_filled_rectangle(posX, posY, posX + TAMANO_CUADRADO, posY + TAMANO_CUADRADO, al_map_rgb(255, 0, 0));
-            al_flip_display();
-        }
-        else if (evento.type == ALLEGRO_EVENT_KEY_DOWN) {
-            switch (evento.keyboard.keycode) {
-                case ALLEGRO_KEY_UP:    tecla_arriba = true; break;
-                case ALLEGRO_KEY_DOWN:  tecla_abajo = true;  break;
-                case ALLEGRO_KEY_LEFT:  tecla_izq = true;    break;
-                case ALLEGRO_KEY_RIGHT: tecla_der = true;    break;
-                case ALLEGRO_KEY_ESCAPE: salir = false;  break;
-            }
-        }
-        else if (evento.type == ALLEGRO_EVENT_KEY_UP) {
-            switch (evento.keyboard.keycode) {
-                case ALLEGRO_KEY_UP:    tecla_arriba = false; break;
-                case ALLEGRO_KEY_DOWN:  tecla_abajo = false;  break;
-                case ALLEGRO_KEY_LEFT:  tecla_izq = false;    break;
-                case ALLEGRO_KEY_RIGHT: tecla_der = false;    break;
-            }
-        }
+        if (al_key_down(&teclado, ALLEGRO_KEY_W)) nuevaY -= VELOCIDAD;
+        if (al_key_down(&teclado, ALLEGRO_KEY_S)) nuevaY += VELOCIDAD;
+        if (al_key_down(&teclado, ALLEGRO_KEY_A)) nuevaX -= VELOCIDAD;
+        if (al_key_down(&teclado, ALLEGRO_KEY_D)) nuevaX += VELOCIDAD;
+        if (al_key_down(&teclado, ALLEGRO_KEY_ESCAPE)) salir = false;
+
+        colision(mapa, &posX, &posY, nuevaX, nuevaY);
+
+        al_clear_to_color(al_map_rgb(10, 10, 10));
+        dibujarMapa(mapa);
+        al_draw_filled_rectangle(posX, posY, posX + TAMANO_CUADRADO, posY + TAMANO_CUADRADO, al_map_rgb(255, 0, 0));
+        al_flip_display();
     }
+    
+}
 
     al_destroy_timer(timer);
     al_destroy_event_queue(queue);
@@ -140,97 +112,89 @@ int main() {
     return 0;
 }
 
-void cargarMapa(char mapa[ALTO][ANCHO]) 
-{
+void cargarMapa(char mapa[ALTO][ANCHO], float *posX, float *posY) {
     FILE *archivofop = fopen("mapa2 copy.txt", "r");
-    int i,j;
     if (!archivofop) {
         fprintf(stderr, "Error al abrir el archivo del mapa.\n");
         for (int i = 0; i < ALTO; i++)
             for (int j = 0; j < ANCHO; j++)
                 mapa[i][j] = '.';
+        *posX = TAMANO_CUADRADO;
+        *posY = TAMANO_CUADRADO;
         return;
     }
 
-    for (i = 0; i < ALTO; i++) {
-        for (j = 0; j < ANCHO; j++) {
+    for (int i = 0; i < ALTO; i++) {
+        for (int j = 0; j < ANCHO; j++) {
             fscanf(archivofop," %c",&mapa[i][j]);
+            if (mapa[i][j] == '@') { // jugador en el mapa
+                *posX = j * TAMANO_CUADRADO;
+                *posY = i * TAMANO_CUADRADO;
+            }
         }
     }
-
     fclose(archivofop);
-    return;
 }
 
-
-void leerMapa(char mapa[ALTO][ANCHO], float *posX, float *posY, float nuevaX, float nuevaY)
-{
+void colision(char mapa[ALTO][ANCHO], float *posX, float *posY, float nuevaX, float nuevaY) {
     float size = TAMANO_CUADRADO;
 
-   
     if (nuevaX != *posX) {
-        float testX = nuevaX;
-        float testY = *posY;
+        int direccion = (nuevaX > *posX) ? 1 : -1;
+        float intentoX = *posX;
+        
+        while (intentoX != nuevaX) {
+            intentoX += direccion;
+            
+            int left   = (int)(intentoX / size);
+            int right  = (int)((intentoX + size - 0.001) / size);
+            int top    = (int)(*posY / size);
+            int bottom = (int)((*posY + size - 0.001) / size);
 
-        float cornersX[2] = { testX, testX + size - 1 };
-        float cornersY[2] = { testY, testY + size - 1 };
-
-        bool colisionH = false;
-        for (int k = 0; k < 2 && !colisionH; k++) {
-            int cellX = (int)(cornersX[k] / TAMANO_CUADRADO);
-            int cellY = (int)(cornersY[0] / TAMANO_CUADRADO);
-            if (cellX < 0 || cellX >= ANCHO || cellY < 0 || cellY >= ALTO) 
-            {
-                colisionH = true;
-                break;
+            bool colision = false;
+            for (int y = top; y <= bottom; y++) {
+                for (int x = left; x <= right; x++) {
+                    if (x < 0 || x >= ANCHO || y < 0 || y >= ALTO || mapa[y][x] == '#') colision = true;
+                }
             }
-            if (mapa[cellY][cellX] == '#') { colisionH = true; break; }
 
-            cellY = (int)(cornersY[1] / TAMANO_CUADRADO);
-            if (cellY < 0 || cellY >= ALTO)
-             { colisionH = true;
-             break; 
-            }
-            if (mapa[cellY][cellX] == '#') 
-            { 
-                colisionH = true;
-                break;
+            if (colision) {
+                intentoX -= direccion;
+                break; 
             }
         }
-
-        if (!colisionH) {
-            *posX = testX;
-        }
+        *posX = intentoX;
     }
 
     if (nuevaY != *posY) {
-        float testX = *posX;
-        float testY = nuevaY;
+        int direccion = (nuevaY > *posY) ? 1 : -1;
+        float intentoY = *posY;
+        
+        while (intentoY != nuevaY) {
+            intentoY += direccion;
+            
+            int left   = (int)(*posX / size);
+            int right  = (int)((*posX + size - 0.001) / size);
+            int top    = (int)(intentoY / size);
+            int bottom = (int)((intentoY + size - 0.001) / size);
 
-        float cornersX[2] = { testX, testX + size - 1 };
-        float cornersY[2] = { testY, testY + size - 1 };
+            bool colision = false;
+            for (int y = top; y <= bottom; y++) {
+                for (int x = left; x <= right; x++) {
+                    if (x < 0 || x >= ANCHO || y < 0 || y >= ALTO || mapa[y][x] == '#') colision = true;
+                }
+            }
 
-        bool colisionV = false;
-        for (int k = 0; k < 2 && !colisionV; k++) {
-            int cellX = (int)(cornersX[0] / TAMANO_CUADRADO);
-            int cellY = (int)(cornersY[k] / TAMANO_CUADRADO);
-            if (cellX < 0 || cellX >= ANCHO || cellY < 0 || cellY >= ALTO) { colisionV = true; break; }
-            if (mapa[cellY][cellX] == '#') { colisionV = true; break; }
-
-            cellX = (int)(cornersX[1] / TAMANO_CUADRADO);
-            if (cellX < 0 || cellX >= ANCHO) { colisionV = true; break; }
-            if (mapa[cellY][cellX] == '#') { colisionV = true; break; }
+            if (colision) {
+                intentoY -= direccion;
+                break;
+            }
         }
-
-        if (!colisionV) {
-            *posY = testY;
-        }
+        *posY = intentoY;
     }
 }
 
-
-void dibujarMapa(char mapa[ALTO][ANCHO])
-{
+void dibujarMapa(char mapa[ALTO][ANCHO]) {
     for (int i = 0; i < ALTO; i++) {
         for (int j = 0; j < ANCHO; j++) {
             if (mapa[i][j] == '#') {
@@ -238,8 +202,6 @@ void dibujarMapa(char mapa[ALTO][ANCHO])
                 float y = i * TAMANO_CUADRADO;
                 al_draw_filled_rectangle(x, y, x + TAMANO_CUADRADO, y + TAMANO_CUADRADO, al_map_rgb(150, 150, 150));
             }
-           // if (mapa[i][j] == '@') {
-
         }
     }
 }
